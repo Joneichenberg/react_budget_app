29229c2eac69c0a01e48dfe595914e9c0fd8ad6e
Jonathan Eichenberg <jon_eichenberg@DESKTOP-5P07OA1.localdomain>
2023-06-08 13:51:32 -0700

    broke ui down into react components

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,16 @@
-
+/*
+ importing app context and the useContext hook, and pass my AppContext to it - this is how a component 
+ connects to the context in order to get values from global state.
+ This is adding text and value to the budget.
+ */
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: ${budget}</span>
+        </div>
+    );
+};
+export default Budget;
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,23 @@
+/*
+using the reduce function to get a total of all the costs, assigning this to a variable 
+and displaying the variable in JSX.
 
+whenever the user adds an expense this causes the state to update which will cause all 
+components connected to the context to re-render and update themselves with new values.
+*/
+
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext'; 
+const Remaining = () => {
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: ${budget - totalExpenses}</span>
+        </div>
+    );
+};
+export default Remaining;
